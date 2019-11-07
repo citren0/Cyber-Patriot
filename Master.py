c36364df6b72044bbfdb11d522b03e407206d89d
Cameron D <42524057+citren0@users.noreply.github.com>
2019-11-07 08:20:35 -0700

    Update Master.py

--- Master.py
+++ Master.py
@@ -11,5 +11,19 @@ print("-----  |   \  |  \   __|__  |        |   ")
 
 commandls = os.popen('ls')
 list = commandls.read().split()
+print(list)
 
-print('')
+
+
+yorn = input("Would you like to run all the scripts? (Y or N) ")
+if 'y' in yorn.lower():
+        print('Running:')
+        for currentCom in list:
+                if 'py' in currentCom:
+                        command = os.popen('sudo python3 ' + currentCom)
+                        command2 = command.read()
+                elif 'sh' in currentCom:
+                        command = os.popen('sudo bash ' + currentCom)
+                        command2 = command.read()
+
+exit("Exiting.")
