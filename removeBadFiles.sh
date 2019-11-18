4de2d7e643d6722b6accd0efaadc99868d379754
Cameron D <42524057+citren0@users.noreply.github.com>
2019-11-18 08:26:00 -0700

    Replaced removeBadFiles.sh

---
+++ removeBadFiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+find / -name \'*.mp3\' -type f -delete
+find / -name \'*.mp4\' -type f -delete
+find / -name \'*.wav\' -type f -delete
+find / -name \'*.avi\' -type f -delete
+find / -name \'*.mpg\' -type f -delete
+find / -name \'*.mpeg\' -type f -delete
+find / -name \'*.flac\' -type f -delete
+find / -name \'*.m4a\' -type f -delete
+find / -name \'*.flv\' -type f -delete
+find / -name \'*.ogg\' -type f -delete
