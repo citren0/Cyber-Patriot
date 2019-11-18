ad5946686c87d28d859221ffcf19cb0b0b6f0208
Cameron D <42524057+citren0@users.noreply.github.com>
2019-11-18 07:58:49 -0700

    Added permissions.sh
    Changes insecure file permissions to probably even less secure file permissions which may or may not give me points.

---
+++ permissions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo 'Run as root.'
+
+chmod 777 /etc/shadow
+chmod 777 /etc/passwd
+cp /etc/shadow /etc/shadow.bk
+cp /etc/passwd /etc/passwd.bk
+
+chmod 640 ~/.bash_history
+chmod 604 /etc/shadow
+chmod 604 /etc/passwd
+chmod 644 /etc/hosts
+chmod 644 /etc/lightdm/lightdm.conf
+chmod 644 /etc/apt/sources.list
