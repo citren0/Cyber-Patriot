fd851bf927412e2c5208152be083117afe7b09c0
Cameron D <42524057+citren0@users.noreply.github.com>
2019-12-02 08:23:57 -0700

    updated removePrograms

--- removePrograms.sh
+++ removePrograms.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-echo 'Run as root.'
+if [[ $EUID -ne 0 ]]; then
+	echo "Run as root."
+	exit 1
+fi
 
-apt purge wireshark zenmap nmap ophcrack hydra-gtk
+sudo apt-get purge wireshark zenmap nmap ophcrack hydra-gtk aisleriot five-or-more four-in-a-row gnome-chess gnome-klotski gnome-mahjongg gnome-mines gnome-nibbles gnome-robots gnome-sudoku 
+sudo apt-get purge gnome-tetravex hitori iagno lightsoff quadrapassel swell-foop tali gnome-hearts
