933c79530f8ab56439f9133fe0a18566cab26a8d
Cameron D <42524057+citren0@users.noreply.github.com>
2020-07-10 11:30:59 -0600

    Update sshNoRoot.sh
    added a restart for the ssh daemon to the sshnoroot file.

--- sshNoRoot.sh
+++ sshNoRoot.sh
@@ -2,3 +2,4 @@
 
 sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sed -i -e 's/PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
+systemctl restart sshd
