26a9d78f4c458ecf7c1661836e5258810bf31cf8
citren0 <alphahak22@gmail.com>
2020-07-15 14:42:52 -0600

    changed sshnoroot to remove comment from prohit root

--- sshNoRoot.sh
+++ sshNoRoot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sed -i -e 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config
 sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sed -i -e 's/PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
 systemctl restart sshd
